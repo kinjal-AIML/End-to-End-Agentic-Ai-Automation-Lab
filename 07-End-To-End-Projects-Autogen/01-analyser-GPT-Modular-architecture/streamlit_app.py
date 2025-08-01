ee2faec45725dc30b00f2cdaa7286c9cd025800c
MDalamin5 <mdal.amin5@northsouth.edu>
2025-08-01 18:41:01 +0600

    allhamdulliah Analyzer GPT is done

--- 07-End-To-End-Projects-Autogen/01-analyser-GPT-Modular-architecture/streamlit_app.py
+++ 07-End-To-End-Projects-Autogen/01-analyser-GPT-Modular-architecture/streamlit_app.py
@@ -22,6 +22,16 @@ st.sidebar.selectbox("Select The Model",["Ollama","GroQ","Openai"])
 
 upload_file = st.file_uploader("",type=["csv"])
 
+## TODO: Create Streamlit Session state.
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    
+if "autogen_team_state" not in st.session_state:
+    st.session_state.autogen_team_state = None
+    
+if ("images_shown") not in st.session_state:
+    st.session_state.images_shown = []
+
 task = st.chat_input("Enter your task hare....")
 
   
@@ -32,6 +42,9 @@ async def run_analyzer_gpt(docker, ollama_model_client, task):
     try:
         await start_docker_container(docker)
         team = getDataAnalyzerTeam(docker, ollama_model_client)
+        
+        if st.session_state.autogen_team_state is not None:
+            await team.load_state(st.session_state.autogen_team_state)
         
         async for messages in team.run_stream(task=task):
             if isinstance(messages, TextMessage):
@@ -45,11 +58,15 @@ async def run_analyzer_gpt(docker, ollama_model_client, task):
                 elif messages.source.startswith("Python_Code_Executor"):
                     with st.chat_message("Code Executor", avatar="🧑‍💻"):
                         st.markdown(messages.content)
-                        
+            
+                st.session_state.messages.append(messages.content)
                         
             elif isinstance(messages, TaskResult):
                 st.markdown(f"Stop Reason is : {messages.stop_reason}")
+                
+                st.session_state.messages.append(messages.stop_reason)
         
+        st.session_state.autogen_team_state = await team.save_state()
         return None
     
     except Exception as e:
@@ -60,6 +77,11 @@ async def run_analyzer_gpt(docker, ollama_model_client, task):
         await stop_docker_container(docker)
 
 
+# TODO: Check the Session State Messages.
+if st.session_state.messages:
+    for msg in st.session_state.messages:
+        st.markdown(msg)
+        
 
 if task:
     if upload_file is not None:
@@ -79,6 +101,12 @@ if task:
             st.error(f"An error occurred: {error}")
             
         if os.path.exists("temp/output.png"):
+            # if("output.png" not in st.session_state.images_shown):
+            #     st.session_state.images_shown.append("output.png")
+            
+            # if "output.png" not in st.session_state.images_shown:
+            #     st.session_state.images_shown.append("output.png")
+            
             st.image("temp/output.png", caption="Output Image")
     else:
         st.error("Please uploaded the file and provide the task.")
