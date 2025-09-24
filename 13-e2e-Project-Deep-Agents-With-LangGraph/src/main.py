699f87ef54eded59873122c146ce2c009e84fe2e
mdalamin5 <mdal.amin5@northsouth.edu>
2025-09-24 22:12:23 +0600

    deep agent start tomorrow

---
+++ 13-e2e-Project-Deep-Agents-With-LangGraph/src/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+async def test_func():
+    print("Project will be start on Tomorrow")
+
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(test_func())
