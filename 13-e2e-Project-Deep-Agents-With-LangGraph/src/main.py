a3d60ba6a4809564f588e1dd66b349dc8032493f
mdalamin5 <mdal.amin5@northsouth.edu>
2025-09-25 10:57:08 +0600

    add new project as content automations

--- 13-e2e-Project-Deep-Agents-With-LangGraph/src/main.py
+++ 13-e2e-Project-Deep-Agents-With-LangGraph/src/main.py
@@ -4,7 +4,11 @@ async def test_func():
     print("Project will be start on Tomorrow")
 
 
-
+app = FastAPI(
+    title="Test Line Api",
+    description="Test line Endpoint",
+    version="1.0.1"
+)
 if __name__ == "__main__":
     import asyncio
     asyncio.run(test_func())
