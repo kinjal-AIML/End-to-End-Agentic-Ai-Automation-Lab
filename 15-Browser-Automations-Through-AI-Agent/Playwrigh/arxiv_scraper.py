540f397980da09fcef4b6e5fc45cac97c51def4a
mdalamin5 <mdal.amin5@northsouth.edu>
2025-10-11 16:50:08 +0600

    arxiv paper serarch and store the data into local dir

--- 15-Browser-Automations-Through-AI-Agent/Playwrigh/arxiv_scraper.py
+++ 15-Browser-Automations-Through-AI-Agent/Playwrigh/arxiv_scraper.py
@@ -14,12 +14,29 @@ with sync_playwright() as p:
 
     # Visit your target URL
     # page.goto("https://www.google.com/search?q=mdalamin5+github")
-    page.goto("https://www.linkedin.com/in/mdalamin5/")
+    # page.goto("https://www.linkedin.com/in/mdalamin5/")
     # page.goto("https://www.linkedin.com/in/abdullahmatin6920/")
     # page.goto("https://www.linkedin.com/in/rubayet-faisal-75806157/")
     # page.goto("https://www.linkedin.com/in/faiaz-hossain-nirob/")
     time.sleep(5)
+    page.goto("https://arxiv.org/search")
+
+    page.get_by_placeholder("Search term...").fill(
+        "Agentic Rag"
+    )
+
+    page.get_by_role("button").get_by_text(
+        "Search"
+    ).nth(1).click()
+
+    links = page.locator(
+        "xpath=//a[contains(@href, 'arxiv.org/pdf')]"
+    ).all()
     
+    for link in links:
+        url = link.get_attribute("href")
+        print(url)
+        urlretrieve(url, "paperData/" + url[-5:] + ".pdf")
 
     # Print page details
     print("Title:", page.title())
@@ -29,7 +46,7 @@ with sync_playwright() as p:
     title = page.title()
 
     # Sanitize filename (avoid special chars)
-    safe_title = "".join(c if c.isalnum() else "_" for c in title)[:60]
+    # safe_title = "".join(c if c.isalnum() else "_" for c in title)[:60]
 
     # # Save the page HTML
     # file_path = f"{safe_title}.html"
