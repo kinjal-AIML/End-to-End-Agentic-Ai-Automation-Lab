3be94d327e8090ebdd61dec97d897a48437e0d73
mdalamin5 <mdal.amin5@northsouth.edu>
2025-12-31 11:28:18 +0600

    advanced pdf building

---
+++ 2.5-LangChain-v1.0.0.1/00-Pdf-builder-reportLab/01-advanced-pdf.py
@@ -0,0 +1,7 @@
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Spacer, Table, Paragraph, SimpleDocTemplate
+from reportlab.graphics.charts.barcharts import VerticalBarChart
+from reportlab.graphics.shapes import Drawing
+
