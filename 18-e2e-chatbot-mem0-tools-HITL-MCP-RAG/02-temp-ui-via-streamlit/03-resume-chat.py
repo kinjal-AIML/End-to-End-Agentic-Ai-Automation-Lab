3c19bb5ec1998b3b5d7bbfefe996f9763a6158ff
mdalamin5 <mdal.amin5@northsouth.edu>
2026-01-18 22:20:31 +0600

    update ui design as add sidebaar and dynamic confg

---
+++ 18-e2e-chatbot-mem0-tools-HITL-MCP-RAG/02-temp-ui-via-streamlit/03-resume-chat.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+import uuid
+
+
+## utility functions
+def generate_thread_id():
+    return uuid.uuid4()
+
+# st.session_state -> dict -> 
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state["thread_id"] = generate_thread_id()
+
+
+# ------------SIDE BAR RELATED UI--------------
+st.sidebar.title("CatBot-BYV")
+st.sidebar.button("New Chat...")
+st.sidebar.header("My Conversations...")
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+#{'role': 'user', 'content': 'Hi'}
+#{'role': 'assistant', 'content': 'Hi=ello'}
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    CONFIG = {'configurable': {'thread_id': st.session_state["thread_id"]}}
+    # first add the message to message_history
+    with st.chat_message('assistant'):
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode= 'messages'
+            )
+        )
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
