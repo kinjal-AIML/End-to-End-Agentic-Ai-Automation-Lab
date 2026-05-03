4a84acb0faf0846f75ed90412f9405afc4eabf53
mdalamin5 <mdal.amin5@northsouth.edu>
2026-05-03 16:52:49 +0600

    implement rate-liming functions

---
+++ 26-Redis-All-You-need-to-Know/02-rate-limiter/main.py
@@ -0,0 +1,29 @@
+import time
+from fastapi import Request, HTTPException
+
+RATE_LIMIT = 5       # requests
+WINDOW_SIZE = 60     # seconds
+
+async def rate_limiter(request: Request):
+    client_ip = request.client.host
+
+    key = f"rate_limit:{client_ip}"
+
+    # Get current count
+    current = await request.app.state.redis.get(key)
+
+    if current is None:
+        # First request → set count = 1 and expiry
+        await request.app.state.redis.set(key, 1, ex=WINDOW_SIZE)
+        return
+
+    current = int(current)
+
+    if current >= RATE_LIMIT:
+        raise HTTPException(
+            status_code=429,
+            detail="Rate limit exceeded. Try again later."
+        )
+
+    # Increment count
+    await request.app.state.redis.incr(key)
