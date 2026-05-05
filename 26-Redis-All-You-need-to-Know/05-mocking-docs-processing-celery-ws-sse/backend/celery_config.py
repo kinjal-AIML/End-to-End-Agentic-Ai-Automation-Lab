6d5027bfb21d55dc6a55492517ab0734aef5700f
mdalamin5 <mdal.amin5@northsouth.edu>
2026-05-05 12:38:20 +0600

    celery config file setup

---
+++ 26-Redis-All-You-need-to-Know/05-mocking-docs-processing-celery-ws-sse/backend/celery_config.py
@@ -0,0 +1,12 @@
+from celery import Celery
+
+celery_app = Celery(
+    "worker",
+    broker="redis://localhost:6379/0",
+    backend="redis://localhost:6379/1"
+)
+
+celery_app.conf.update(
+    task_track_started=True,
+    worker_prefetch_multiplier=1,
+)
